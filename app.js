1d33efe3cb3fd73a76792b6a705e4ef3c12c719a
Volodymyr Dziubanskyi <volodumur.dzyubanskyy@gmail.com>
2018-06-13 07:01:28 +0000

    add mongo db

--- app.js
+++ app.js
@@ -1,44 +1,90 @@
-var express=require("express");
-var app=express();
-var bodyParser= require("body-parser");
+var express=require("express"),
+    app=express(),
+    bodyParser= require("body-parser"),
+    mongoose=require("mongoose");
 
-var campgrounds=[
-            {name:"Vova", img: "http://www.photosforclass.com/download/flickr-3753652218"},
-            {name:"Vova1", img: "http://www.photosforclass.com/download/flickr-14435096036"},
-            {name:"Vova2", img: "http://www.photosforclass.com/download/flickr-2182093741"},
-            {name:"Vova3", img: "http://www.photosforclass.com/download/flickr-7121861565"},            {name:"Vova", img: "http://www.photosforclass.com/download/flickr-3753652218"},
-            {name:"Vova1", img: "http://www.photosforclass.com/download/flickr-14435096036"},
-            {name:"Vova2", img: "http://www.photosforclass.com/download/flickr-2182093741"},
-            {name:"Vova3", img: "http://www.photosforclass.com/download/flickr-7121861565"},            {name:"Vova", img: "http://www.photosforclass.com/download/flickr-3753652218"},
-            {name:"Vova1", img: "http://www.photosforclass.com/download/flickr-14435096036"},
-            {name:"Vova2", img: "http://www.photosforclass.com/download/flickr-2182093741"},
-            {name:"Vova3", img: "http://www.photosforclass.com/download/flickr-7121861565"},            {name:"Vova", img: "http://www.photosforclass.com/download/flickr-3753652218"},
-            {name:"Vova1", img: "http://www.photosforclass.com/download/flickr-14435096036"},
-            {name:"Vova2", img: "http://www.photosforclass.com/download/flickr-2182093741"},
-            {name:"Vova3", img: "http://www.photosforclass.com/download/flickr-7121861565"}
-]; 
+mongoose.connect("mongodb://localhost/yelp_camp");
+// SCHEMA SETUP
+
+var campgroudsSchema=new mongoose.Schema({
+    name: String,
+    img: String,
+    descr: String
+});
+
+var Campground = mongoose.model("Campground", campgroudsSchema);
+
+//Create a NEW CAMPGROUND
+//====================================================================
+// Campground.create({
+//         name: "Glacier Basin Campground",
+//         img: "https://www.nps.gov/common/uploads/structured_data/B51D1028-1DD8-B71B-0BD70D7C96AD7D46_thumb.jpg",
+//         descr: "A pleasant mix of Douglas fir, Lodgepole pine, Ponderosa pine, and the occasional Engelmann spruce forests the campground, offering equal amounts of sun and shade. Grasses, shrubs and seasonal wildflowers fill the open meadows."
+//     },
+//     function(err, newlyCamp) {
+//         if(err){
+//             console.log("something wrong");
+//         }else{
+//             console.log("add OK");
+//         }
+//     });
+
+//====================================================================
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.render("index");
+    res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds}    );    
+    
+    //Get All campground from DB
+    
+    Campground.find({}, function(err, allcampgrounds){
+        if(err){
+            console.log(err);            
+        }else{
+            res.render("index", {campgrounds: allcampgrounds}    );
+        }
+    });
+    
+        
 });
 
 app.post("/campgrounds", function(req, res){
     var name=req.body.name;
-    var image=req.body.image;
-    var newCampg={name: name, img: image};
-    campgrounds.push(newCampg);
-   res.redirect("/campgrounds"); 
+    var img=req.body.img;
+    var descr=req.body.descr;
+    var newCampg={name: name, img: img, descr: descr};
+   //Create a new campgrount
+   Campground.create(newCampg, function(err, newlyCamp){
+       if(err){
+           console.log(err)
+       }else{
+            res.redirect("/campgrounds"); 
+       }
+   });
 });
 
 app.get("/campgrounds/new", function(req, res) {
    res.render("new");
+});
+
+// SHOW s more info
+app.get("/campgrounds/:id", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCamp){
+        if(err){
+            console.log("errr");
+        }else{
+            res.render("show", {campground: foundCamp});
+        }
+        
+    });
+    
+    
+   
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
